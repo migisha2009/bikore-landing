daa82147b801f655eed951069245a4db51259ff1
johnson m <migishajohnson24gmail.com>
2026-05-03 09:40:18 +0200

    allowed vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
